5940550761e40331ebb39cfbad5e6397b56ea1e9
Marcos Alberto <159904717+marcosffp@users.noreply.github.com>
2024-11-11 19:15:39 -0300

    anexação de arquivos

--- 02-Modulo/src/hello.ts
+++ 02-Modulo/src/hello.ts
@@ -78,5 +78,57 @@ function linguagem(linguagens:string[]) {
 //Tuplas
 let pessoa: [string, number, boolean] = ["Marcos", 30, true];
 
+let listaFrutas:[string,...string[]]=["Maçã", "Banana", "Pera"];
+console.log(...listaFrutas);
+
+let pessoas:[nome: string, idade: number, email: string]=["Marcos", 30, "marcosalberto@gmail.com"];
+
+type Pessoa=[string, number, boolean]|[string, number, boolean];
+
+
+//Enum
+enum Cor{
+  VERMELHO,
+  AZUL,
+  AMARELO
+}
+console.log(Cor);
+
+let corFavorita: Cor = Cor.AZUL;
+console.log(corFavorita);
+
+
+//Any
+let valor: any = 10;
+console.log(valor);
+const a:any=888;
+const b: any=['M'];
+const c: any=a+b;
+console.log(c);//CUIDADOOOOOOO
+
+//Unknow
+let valor2: unknown = 10;
+console.log(valor2);
+let valorVariavel: unknown;
+valorVariavel=true;
+valorVariavel=10;
+valorVariavel=[];
+console.log(valorVariavel);
+
+//let valor1: boolean=valorVariavel; TÁ ERRADO
+
+
+//Void
+function mostrarMensagem(mensagem: string): void{
+  console.log(mensagem);
+}
+
+const olaMundo=(): void =>{
+  console.log("Olá, Mundo!")
+};
+
+
+
+
 
 
